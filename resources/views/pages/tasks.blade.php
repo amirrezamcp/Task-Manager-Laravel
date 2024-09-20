19357b67b9bf6f4e2d492bad2043e02573f7e14c
amirrezamcp <mousavi.amirreza30@gmail.com>
2024-09-20 22:48:05 +0330

    Show Tasks

--- resources/views/pages/tasks.blade.php
+++ resources/views/pages/tasks.blade.php
@@ -3,21 +3,18 @@
 @section('content')
     <div class="content">
         <h2>لیست تسک‌ها</h2>
-        <div class="add-task">
-            <input type="text" id="taskInput" placeholder="عنوان تسک جدید">
-            <button onclick="addTask()">اضافه کردن</button>
-        </div>
-        <ul class="task-list" id="taskList">
-            <li class="task-item">تسک 1</li>
-            <li class="task-item">تسک 2</li>
-            <li class="task-item">تسک 3</li>
-            <li class="task-item">تسک 4</li>
-            <li class="task-item">تسک 5</li>
-            <li class="task-item">تسک 6</li>
-            <li class="task-item">تسک 7</li>
-            <li class="task-item">تسک 8</li>
-            <li class="task-item">تسک 9</li>
-            <li class="task-item">تسک 10</li>
-        </ul>
+        <form action="#" method="POST" id="taskForm">
+            @csrf
+            <div class="add-task">
+                <input type="text" id="taskInput" placeholder="عنوان تسک جدید">
+                <button onclick="addTask()">اضافه کردن</button>
+            </div>
+            <ul class="task-list" id="taskList">
+                @foreach ($tasks as $task)
+                <li class="task-item"> {{ $task->title }} </li>
+                @endforeach
+            </ul>
+        </form>
     </div>
+    <script src="{{ asset('asset/js/tasks.js') }}"></script>
 @endsection
