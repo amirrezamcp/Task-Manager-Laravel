d6a95f86a2250b6a3487ed56f697b137d55ee73c
amirrezamcp <mousavi.amirreza30@gmail.com>
2024-09-27 16:30:42 +0330

    Update Menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,6 +1,7 @@
 <nav>
-    <a href="#">تسک‌ها</a>
+    <a href=" {{ route('tasks_show') }} ">تسک‌ها</a>
     <a href="#">پروژه‌ها</a>
     <a href="#">تنظیمات</a>
-    <a href="/login">ورود</a>
+    <a href=" {{ route('login_show') }} ">ورود</a>
+    <a href="{{ route('register_show') }}">ثبت نام</a>
 </nav>
