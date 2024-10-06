09b1609093a249dd73c9b8b8adc15902da7b65b1
amirrezamcp <mousavi.amirreza30@gmail.com>
2024-10-06 11:55:22 +0330

    check guard navbar

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,8 +1,12 @@
 <nav>
-    <a href=" {{ route('Home_page') }} ">صفحه اصلی</a>
-    <a href=" {{ route('tasks_show') }} ">تسک‌ها</a>
-    <a href="#">تنظیمات</a>
-    <a href=" {{ route('login_show') }} ">ورود</a>
-    <a href="{{ route('register_show') }}">ثبت نام</a>
-    <a href="{{ route('logout') }}">خروج</a>
+    @if (auth()->guard('peoples')->check())
+        <a href=" {{ route('Home_page') }} ">صفحه اصلی</a>
+        <a href=" {{ route('tasks_show') }} ">تسک‌ها</a>
+        <a href="#">تنظیمات</a>
+        <a href="{{ route('logout') }}">خروج</a>
+    @else        
+        <a href=" {{ route('Home_page') }} ">صفحه اصلی</a>
+        <a href=" {{ route('login_show') }} ">ورود</a>
+        <a href="{{ route('register_show') }}">ثبت نام</a>
+    @endif
 </nav>
