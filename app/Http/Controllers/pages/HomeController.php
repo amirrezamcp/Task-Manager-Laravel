dc0c03beb62d9749504d9cddc6057460a924b66c
amirrezamcp <mousavi.amirreza30@gmail.com>
2024-10-11 22:22:45 +0330

    Show statistics

--- app/Http/Controllers/pages/HomeController.php
+++ app/Http/Controllers/pages/HomeController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers\pages;
 
 use App\Http\Controllers\Controller;
+use App\Models\People;
+use App\Models\Tasks;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke(Request $request)
     {
-        return view('pages.home');
+        $totalTasks = Tasks::count();
+        $completedTasks = Tasks::where('is_done', 1)->count();
+        $pendingTasks = Tasks::where('is_done', 0)->count();
+        $users = People::count();
+
+        return view('pages.home', compact('totalTasks', 'completedTasks', 'pendingTasks', 'users'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
     <div class="content">
-        <h2> به تسک منیجر خوش امدید </h2>
+        <h2>به تسک منیجر خوش آمدید</h2>
+
+        <div class="container">
+            <h1>آمار تسک‌ها</h1>
+            <div class="chart-container">
+                <canvas id="taskChart"></canvas>
+            </div>
+            <h2>تعداد کاربران: {{ $users }}</h2>
+            <input type="hidden" id="totalTasks" value="{{ $totalTasks }}">
+            <input type="hidden" id="completedTasks" value="{{ $completedTasks }}">
+            <input type="hidden" id="pendingTasks" value="{{ $pendingTasks }}">
+        </div>
 
         <div class="login-redirect">
-            <p> حساب کاربری نداری؟ <a href="{{ route('register_show') }}"> ساخت حساب </a></p>
+            <p>حساب کاربری نداری؟ <a href="{{ route('register_show') }}">ساخت حساب</a></p>
             <p>قبلاً حساب دارید؟ <a href="{{ route('login_show') }}">ورود به حساب</a></p>
         </div>
     </div>
